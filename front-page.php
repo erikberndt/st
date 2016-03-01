119ebc4cc6d2b6131958b0bdaf93088fcb07f015
erikberndt <erikberndt@gmail.com>
2016-03-01 20:51:42 +0100

    First commit - front-page.php

---
+++ front-page.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Startseite der Gebrueder-Koerting Schule
+ *
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="clearfix sm-flex lg-mxn1 ">
+    <div class="col col-12 sm-col-8 py2 px1">
+      <div class="clearfix">
+
+        <!-- Hero -->
+        <div class="col col-12 relative center">
+          <img src="<?php bloginfo( 'template_directory' ); ?>/assets/hero__start.gif" alt="Startbild bewegt" class="md-show">
+          <img
+            src="<?php bloginfo( 'template_directory' ); ?>/assets/hero__start-xs-640.png"
+            srcset="<?php bloginfo( 'template_directory' ); ?>/assets/hero__start-xs-640.png 1x, <?php bloginfo( 'template_directory' ); ?>/assets/hero__start-xs-1280.png 2x"
+            class="inline-block md-hide" alt="Startbild mobil"
+           />
+          <div class="hero__clip-box">
+            <p>Die inklusive Grundschule im Grünen</p>
+            <a href="http://vimeo.com/36031564" class="white fancybox-media"><button class="btn btn-primary mt1 round-btn bg-light-green">Film ab</button></a>
+          </div>
+          <h1 class="center sm-left">Hallo, herzlich Willkommen. </h1>
+          <p class="center sm-left">Schön, dass wir unsere Schule vorstellen können. Unsere Schule – <strong>eine Schule für Alle</strong>. Die Gebrüder-Körting-Schule gibt es seit dem Jahre 2002. Eigentlich eine ganz normale Stadtteilschule im Stadtteil Badenstedt in Hannover. Wer sich unsere Schule jedoch genau anschaut, wird schnell merken, dass es sehr viele Besonderheiten gibt.</p>
+          <hr class="mt3 mb3">
+        </div>
+        <!-- Ende Hero -->
+
+        <!-- Besonderheiten -->
+        <div class="col col-12">
+          <div class="clearfix mxn1">
+
+            <!-- Schülerinterview -->
+            <div class="col col-12 sm-col-6 md-col-4 px1 mb3 feature__box">
+              <div class="">
+                <img src="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Schülerinterview-1280.jpg"
+                srcset="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Schülerinterview-300.jpg 300w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Schülerinterview-600.jpg 600w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Schülerinterview-768.jpg 768w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Schülerinterview-1024.jpg 1024w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Schülerinterview-1280.jpg 1280w  "
+                sizes="(min-width: 40em) 14em, 40em" alt="Schülerinterview">
+              </div>
+              <h3>Schülerinterview</h3>
+              <p class="feature__desc">Hier erklären Ihnen die Schüler selbst die Gebrüder-Körting-Schule.</p>
+            </div>
+
+            <!-- Gesundes Essen -->
+            <div class="col col-12 sm-col-6 md-col-4 px1 mb3 feature__box">
+              <div class="">
+                <img src="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/GesundesEssen-1280.jpg"
+                srcset="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/GesundesEssen-300.jpg 300w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/GesundesEssen-600.jpg 600w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/GesundesEssen-768.jpg 768w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/GesundesEssen-1024.jpg 1024w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/GesundesEssen-1280.jpg 1280w  "
+                sizes="(min-width: 40em) 14em, 40em" alt="Gesundes Essen">
+              </div>
+              <h3>Gesundes Essen</h3>
+              <p class="feature__desc">Jeden Tag gemeinsames, gesundes Essen für alle.</p>
+            </div>
+
+            <!-- Lernentwicklungsbaum -->
+            <div class="col col-12 sm-col-6 md-col-4 px1 mb3 feature__box">
+              <div class="">
+                <img src="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Lernentwicklungsbaum-1280.png"
+                srcset="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Lernentwicklungsbaum-300.png 300w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Lernentwicklungsbaum-600.png 600w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Lernentwicklungsbaum-768.png 768w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Lernentwicklungsbaum-1024.png 1024w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Lernentwicklungsbaum-1280.png 1280w  "
+                sizes="(min-width: 40em) 14em, 40em" alt="Lernentwicklungsbaum">
+              </div>
+              <h3>Lernentwicklungsbaum</h3>
+              <p class="feature__desc">Unsere Schüler kennen und entwickeln ihre Kompetenzen individuell.</p>
+            </div>
+
+            <!-- Tolle AGs  -->
+            <div class="col col-12 sm-col-6 md-col-4 px1 mb3 feature__box">
+              <div class="">
+                <img src="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/TolleAGs-1280.png"
+                srcset="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/TolleAGs-300.png 300w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/TolleAGs-600.png 600w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/TolleAGs-768.png 768w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/TolleAGs-1024.png 1280w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/TolleAGs-1280.png 1280w  "
+                sizes="(min-width: 40em) 14em, 40em" alt="Tolle AGs">
+              </div>
+              <h3>Tolle AGs</h3>
+              <p class="feature__desc">Von Modellbau bis 1. Hilfe - ein breites Angebot wartet hier auf die Schüler.</p>
+            </div>
+
+            <!-- Schule im Grünen -->
+            <div class="col col-12 sm-col-6 md-col-4 px1 mb3 feature__box">
+              <div class="">
+                <img src="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/SchuleImGrünen-1280.jpg"
+                srcset="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/SchuleImGrünen-300.jpg 300w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/SchuleImGrünen-600.jpg 600w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/SchuleImGrünen-768.jpg 768w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/SchuleImGrünen-1024.jpg 1024w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/SchuleImGrünen-1280.jpg 1280w  "
+                sizes="(min-width: 40em) 14em, 40em" alt="Schule im Grünen">
+              </div>
+              <h3>Schule im Grünen</h3>
+              <p class="feature__desc">In den zwei schuleigenen Gärten säen, jäten und ernten unsere Schüler fleißig.</p>
+            </div>
+
+            <!-- Klassenrat -->
+            <div class="col col-12 sm-col-6 md-col-4 px1 mb3 feature__box">
+              <div class="">
+                <img src="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Klassenrat-1280.jpg"
+                srcset="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Klassenrat-300.jpg 300w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Klassenrat-600.jpg 600w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Klassenrat-768.jpg 768w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Klassenrat-1024.jpg 1024w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Klassenrat-1280.jpg 1280w  "
+                sizes="(min-width: 40em) 14em, 40em" alt="Klassenrat">
+              </div>
+              <h3>Klassenrat</h3>
+              <p class="feature__desc">Basis-demokratische Erziehung und Verantwortungsübernahme von Anfang an.</p>
+            </div>
+
+            <!-- Rhytmisierung -->
+            <div class="col col-12 sm-col-6 md-col-4 px1 mb3 feature__box">
+              <div class="">
+                <img src="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Rhythmisierung-1280.jpg"
+                srcset="<?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Rhythmisierung-300.jpg 300w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Rhythmisierung-600.jpg 600w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Rhythmisierung-768.jpg 768w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Rhythmisierung-1024.jpg 1024w, <?php bloginfo( 'template_directory' ); ?>/assets/besonderheiten/Rhythmisierung-1280.jpg 1280w  "
+                sizes="(min-width: 40em) 14em, 40em" alt="Schule im Grünen">
+              </div>
+              <h3>Rhytmisierung</h3>
+              <p class="feature__desc">Kinder brauchen Zeit zum Lernen.</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- Seitenleiste -->
+    <aside class="col col-12 sm-col-4 sm-px1 flex">
+      <div class="clearfix flex-full bg-light-gray">
+        <?php get_template_part('sidebars/sidebar-front'); ?>
+      </div>
+    </aside>
+    <!-- Ende Seitenleiste  -->
+
+
+  </div>
+</div>
+
+<?php get_footer(); ?>
